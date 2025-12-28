bdd67cdac8f27eb928a960175678999c42f71db9
Timothy Miller <innerlogic4321@gmail.com>
2025-12-27 19:45:29 -0500

    Add browser fonts

--- avalonia/DaCardsAV.Browser/Program.cs
+++ avalonia/DaCardsAV.Browser/Program.cs
@@ -2,14 +2,36 @@
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
+using Avalonia.Media;
 using DaCardsAV;
 
 internal sealed partial class Program
 {
-    private static Task Main(string[] args) => BuildAvaloniaApp()
-            .WithInterFont()
-            .StartBrowserAppAsync("out");
+        private static Task Main(string[] args) => BuildAvaloniaApp()
+                .WithInterFont()
+                .With(
+                      new FontManagerOptions
+                      {
+                              DefaultFamilyName = "avares://DaCardsAV.Browser/Assets#Noto Sans",
+                              FontFallbacks = new[]
+                            {
+                                new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://DaCardsAV.Browser/Assets#Noto Sans"),
+                                },
+                                new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://DaCardsAV.Browser/Assets#Noto Mono"),
+                                },
+                                new FontFallback
+                                {
+                                        FontFamily = new FontFamily("avares://DaCardsAV.Browser/Assets#OpenMoji"),
+                                        UnicodeRange = UnicodeRange.Parse("U+23??, U+26??, U+2700-27BF, U+2B??, U+1F1E6-1F1FF, U+1F300-1F5FF, U+1F600-1F64F, U+1F680-1F6FF, U+1F9??")
+                                }
+                            },
+                      })
+                .StartBrowserAppAsync("out");
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>();
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>();
 }
